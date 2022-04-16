70e89c88e2c6c8ac52d10f48be039db080d305e4
Brandon! <brandoge91@gmail.com>
2022-04-16 16:12:57 -0400

    fixed audio for chromenium/safari browsers

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ $.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
 });
 window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
     // play the saponge.mp3 file
-    document.getElementById('myAudio').play();
-    document.getElementById('myAudio').loop = true;
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if (!isChrome){
+        $('#iframeAudio').remove()
+    }
+    else {
+        $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+    }
 
 }
