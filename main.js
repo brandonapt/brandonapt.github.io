65c882b2dc056028df7b25f22503bfd7dcd902a4
Brandon! <brandoge91@gmail.com>
2022-04-16 15:08:44 -0400

    fix

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
   document.getElementById('ip').innerHTML = "<b>"+data.ip;
   document.title = data.ip;
 });
+window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
     // play the saponge.mp3 file
     document.getElementById('myAudio').play();
     document.getElementById('myAudio').loop = true;
