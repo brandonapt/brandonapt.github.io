942ea0573ef176f46fcdedbd69cc6cd8fec7a44b
Brandon! <brandoge91@gmail.com>
2022-04-17 09:22:44 -0400

    fix audio

--- main.js
+++ main.js
@@ -7,11 +7,7 @@ window.onload = function() {
   window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
       // play the saponge.mp3 file
       var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
-      if (!navigator.userAgent.toLowerCase().includes('chrom') && !isSafari) {
-          $('#iframeAudio').remove()
-      }
-      else {
           $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-      }
+      
   
   }
