fba799a2e9e3504b798a2b48b30578d992ac1288
Brandon! <brandoge91@gmail.com>
2022-04-17 07:24:35 -0400

    ANOTHER AUDIO FIX

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 window.onload = function() {
 
-$.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
-  document.getElementById('ip').innerHTML = "<b>"+data.ip;
-  document.title = data.ip;
-});
-window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
-    // play the saponge.mp3 file
-    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)
-    if ( navigator.vendor.toLowerCase().includes('apple')) {
-      document.body.addEventListener('touchstart', function() {
-        alert('safari')
-        $('#iframeAudio').remove()
-        var audio = new Audio('sponge.mp3');
-        audio.play();
-        audio.loop = true
-      }, false);
-    }
-       $('#playAudio').remove() 
-
-}
+  $.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
+    document.getElementById('ip').innerHTML = "<b>"+data.ip;
+    document.title = data.ip;
+  });
+  window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
+      // play the saponge.mp3 file
+      var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+      var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+      if (!isChrome && !isSafari) {
+          $('#iframeAudio').remove()
+      }
+      else {
+          $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+      }
+  
+  }
