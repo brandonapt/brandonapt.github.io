b8da190bc772df298979eb6f45195b1fbd6a13ff
Brandon! <brandoge91@gmail.com>
2022-04-17 09:23:29 -0400

    ik

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.onload = function() {
   window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
       // play the saponge.mp3 file
       var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
-          $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
       
   
   }
